993717205e978763e4288e586b944dd0ea0e97e1
gurjotsinghaulakh <s351873@oslomet.no>
2022-10-29 15:36:34 +0200

    Oppdatert User Model og slettet unødvendige Models

---
+++ TastyTrading/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+
+namespace TastyTrading.Models
+{
+    [ExcludeFromCodeCoverage]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string PostalCode { get; set; }
+        public string Postallocation { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
